464d4154eb88d0b3641f0489fdf976cb02410b2f
unknown <zhumenggang@163.com>
2018-03-22 15:11:28 +0800

    2018.3.22

--- cquptsichuanginfo_collection4/src/main/java/Main/MainClass.java
+++ cquptsichuanginfo_collection4/src/main/java/Main/MainClass.java
@@ -1,14 +1,12 @@
 package Main;
 
 import cqupt.db.DBManager;
-import model.FormatDate;
 import model.RegularPoints;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
 import java.util.Scanner;
 
 public class MainClass {
